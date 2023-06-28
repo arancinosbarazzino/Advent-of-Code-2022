7dbecbd3e6d2a40c693fd290284ba899638878f9
arancinosbarazzino <samueleferrillo16@gmail.com>
2023-06-28 13:24:54 +0200

    Seconda parte giorno 6

--- Giorno6/Giorno6.java
+++ Giorno6/Giorno6.java
@@ -9,7 +9,7 @@ import java.util.Set;
 public class Giorno6 {
 
   public static int primaParte() throws IOException {
-    char[] caratteri= new char[4];
+    char[] caratteri = new char[4];
     int i = 0;
     int conta = 0;
     char carattere;
@@ -24,7 +24,7 @@ public class Giorno6 {
         i = 0;
       }
       carattere = (char) br.read();
-      caratteri[i++]=carattere;
+      caratteri[i++] = carattere;
       if (conta > 3 && controlloDuplicati(caratteri)) {
         br.close();
         return conta;
@@ -37,7 +37,7 @@ public class Giorno6 {
 
   public static boolean controlloDuplicati(char[] array) {
     Set<Character> temp = new HashSet<Character>();
-    for(int i=0;i<array.length;i++){
+    for (int i = 0; i < array.length; i++) {
       temp.add(array[i]);
     }
     if (temp.size() == array.length) {
@@ -46,7 +46,35 @@ public class Giorno6 {
     return false;
   }
 
+  public static int secondaParte() throws IOException {
+    char[] caratteri = new char[14];
+    int i = 0;
+    int conta = 0;
+    char carattere;
+    BufferedReader br = new BufferedReader(
+      new FileReader(
+        "C:\\Users\\Samu\\Documents\\vscode\\Advent-of-Code-2022\\Giorno6\\file.txt"
+      )
+    );
+    while (br.ready()) {
+      conta++;
+      if (i == 14) {
+        i = 0;
+      }
+      carattere = (char) br.read();
+      caratteri[i++] = carattere;
+      if (conta > 13 && controlloDuplicati(caratteri)) {
+        br.close();
+        return conta;
+      }
+    }
+    System.out.println("finito file");
+    br.close();
+    return -1;
+  }
+
   public static void main(String[] args) throws IOException {
     // System.out.println(primaParte());
+    System.out.println(secondaParte());
   }
 }
