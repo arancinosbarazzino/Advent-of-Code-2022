42ec355659c5b34181f776f52ff7ec12008f6c72
arancinosbarazzino <samueleferrillo16@gmail.com>
2023-06-28 22:27:02 +0200

    Provato a fare giorno 7 pt.1 (fallito miseramente)

--- Giorno7/Giorno7.java
+++ Giorno7/Giorno7.java
@@ -4,48 +4,88 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashSet;
+import java.util.Stack;
 
 public class Giorno7 {
-
+//RICORDATI DI FIXARE STA COSA: POSSONO ESSERCI CARTELLE CHE ENTRANO DENTRO CARTELLE CON LO STESSO NOME DEL PADRE
   public static int primaParte(BufferedReader br) throws IOException {
-    HashMap<String, Integer> directories=CreaDirectories(br);
-    int somma=0;
-    for(Integer i : directories.values()){
-        if((int)i < 100000){
-            somma+=(int)i;
-        }
+    HashMap<String, Integer> directories = CreaDirectories(br);
+    directories.toString();
+    int somma = 0;
+    for (Integer i : directories.values()) {
+      if (i.intValue() <= 100000) {
+        somma += i.intValue();
+      }
     }
     return somma;
   }
 
-  public static HashMap<String, Integer> CreaDirectories(BufferedReader br) throws IOException {
+  public static HashMap<String, Integer> CreaDirectories(BufferedReader br)
+    throws IOException {
+    HashMap<String, HashSet<String>> files = new HashMap<String, HashSet<String>>();
     HashMap<String, Integer> directories = new HashMap<String, Integer>(); //una mappa che contiene le directory con le relative dimensioni
-    String directory;
-    Queue<String> oldDirectories = new LinkedList<String>(); //una coda che contiene tutte le directory in cui sono stato
+    Stack<String> oldDirectories = new Stack<String>(); //una coda che contiene tutte le directory in cui sono stato
+    String directoryAttuale = "/";
+    String directoryDir;
+    String file;
+    String temp;
+    String linea;
+    int dimensioneFile;
     while (br.ready()) {
-      String linea = br.readLine();
+      linea = br.readLine(); //leggo una linea del file
       if (linea.startsWith("$ cd")) {
-        directory = linea.substring(5);
-        oldDirectories.add(directory); //aggiungo sempre le directory in cui entro alla queue in modo da poter tornare indietro se serve
-        if (directory == "..") { //caso in cui '$cd ..'
-          directory = oldDirectories.poll();
-          if (directory == null) { //se la directory è null non c'è niente più sopra quindi sono per forza a "/"
-            directory = "/";
-          }
-        } else if (directory == "/") { //caso in cui '$cd /'
-          oldDirectories.clear();
-          directory = "/";
-        } else if (!directories.containsKey(directory)) { //aggiungo la directory se non c'è già
-          directories.put(directory, 0);
+        directoryAttuale = linea.substring(5);
+        if (!directoryAttuale.equals("..") && directoryAttuale != null) {
+          oldDirectories.push(directoryAttuale); //aggiungo le directory che diventeranno le precedenti
         }
-      } else if (linea.startsWith("dir")) {
-        directory = linea.substring(4);
-        if (!directories.containsKey(directory)) { //aggiungo la directory se non c'è già
-          directories.put(directory, 0);
+        if (directoryAttuale.equals("..")) { //caso in cui '$cd ..'
+          files.put(directoryAttuale, null);
+          directoryAttuale = oldDirectories.pop();
+        } else if (directoryAttuale.equals("/")) { //caso in cui '$cd /'
+          oldDirectories.clear();
+          if (directories.get(directoryAttuale) == null) { //aggiungo "/" a directories solo se non c'è già
+            directories.put(directoryAttuale, 0);
+          }
+          if (files.get(directoryAttuale) == null) {
+            files.put(directoryAttuale, new HashSet<String>());
+          } else {
+            files.put(directoryAttuale, files.get(directoryAttuale));
+          }
+        } else if (!directories.containsKey(directoryAttuale)) { //aggiungo la directory se non c'è già
+          directories.put(directoryAttuale, 0);
+          files.put(directoryAttuale, new HashSet<String>());
+        }
+      } else if (linea.startsWith("dir")) { //caso in cui c'è una directory ESEMPIO: dir fajfaj
+        directoryDir = linea.substring(4);
+        if (!directories.containsKey(directoryDir)) { //se la directory non esiste ancora la creo
+          directories.put(directoryDir, 0);
+        }
+      } else if (!linea.startsWith("$ ls")) { //caso in cui c'è un file ESEMPIO: 152491 pisello
+        dimensioneFile =
+          Integer.parseInt(linea.substring(0, linea.indexOf(" "))); //ottengo dimensione del file
+        file = linea.substring(linea.indexOf(" ") + 1, linea.length()); //ottengo nome del file
+        if (
+          files.get(directoryAttuale) != null &&
+          !(files.get(directoryAttuale).contains(file))
+        ) { //se non ho già trovato questo file in questa directory allora lo aggiungo
+          //NB: LO STESSO FILE PUÒ TROVARSI IN + DIRECTORY CON DIMENSIONI DIVERSE!!!!!
+          files.get(directoryAttuale).add(file);
+          int somma;
+          Stack<String> appoggio = new Stack<String>();
+          while (!oldDirectories.isEmpty()) { //aggiungo la dimensione del file a tutte le cartelle padri della cartella attuale
+            somma = 0;
+            temp = oldDirectories.pop();
+            somma = directories.get(temp);
+            somma += dimensioneFile;
+            directories.replace(temp, somma);
+            appoggio.push(temp);
+          }
+          oldDirectories.addAll(appoggio);
+          appoggio.clear();
         }
       }
+      linea = br.readLine();
     }
     return directories;
   }
@@ -53,5 +93,9 @@ public class Giorno7 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new FileReader("Giorno7/file.txt"));
     System.out.println(primaParte(br));
+    // HashMap<String, Integer> directories = CreaDirectories(br);
+    // if (directories.get("/") != null) {
+    //   System.out.println("pise");
+    // }
   }
 }
